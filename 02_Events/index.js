4c1c9e2cfcb977629bca6ae6afea110a42994686
moiz-xyz <a.moizniaxi123@gmail.com>
2025-08-26 23:20:16 +0500

    added events in socket

---
+++ 02_Events/index.js
@@ -0,0 +1,30 @@
+import express from "express"
+import path from "path"
+import { fileURLToPath } from 'url';
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+const PORT =  8000
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join( __dirname, "client/index.html"))  
+})
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log(" message", msg)
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+
